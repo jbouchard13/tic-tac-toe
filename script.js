351e256e8fb1efc0d6113b2255b17a28e42c2bbd
John Bouchard <jbouchard13@gmail.com>
2022-04-14 22:19:43 -0400

    created grid layout for gameboard

---
+++ script.js
@@ -0,0 +1,10 @@
+// create a module for the gameboard
+const GameBoard = (() => {
+  // gather the 9 boxes and put them into an array
+})();
+
+// create a module for game controls
+const Controller = (() => {})();
+
+// create a factory for player objects
+const Player = (playerName) => {};
